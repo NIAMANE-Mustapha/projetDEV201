e08cb68365566da0dded8fae11020758d0c3f645
DEV201 <user@gmail.com>
2024-11-15 09:51:41 +0100

    ajouyter input

--- src/Composants/Somme/Multi.jsx
+++ src/Composants/Somme/Multi.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Multi(props) {
   return (
     <div>
+        <input type="text" name="" id="" />
         <input type="text" name="" id="" 
         value={props.msg} 
         onChange={e=>props.setmsg(e.target.value)}
